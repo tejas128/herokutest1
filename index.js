24fd5e0d8827494025556d659eecd52090223b27
tejas128 <ladtejas128@gmail.com>
2021-08-24 15:50:32 +0530

    additon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express= require("express")
 const app = express()
 const apirouter=require("./routes/routes")
 app.use("/",apirouter)
-app.listen(3000,()=>{
+app.listen(process.env.port || 3000,()=>{
     console.log("server started .")
 })
 
