82cec38b23fad20ea59e6d21a83e0ceab938859c
tejas128 <ladtejas128@gmail.com>
2021-08-24 16:08:30 +0530

    changes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express= require("express")
-
+const dotenv=require('dotenv')
 const app = express()
 const apirouter=require("./routes/routes")
+dotenv.config()
 app.use("/",apirouter)
-app.listen(process.env.port || 3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("server started .")
 })
 
